b35cc1b2ea1a458647d1d39b095479815dc5285d
Nizar Noorani <nizar.noorani@gmail.com>
2015-01-13 09:16:51 -0600

    modifies image sizes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -29,9 +29,9 @@ attachment_config = {
 
   styles: {
       mini:     "48x48>",
-      small:    "100x100>",
-      product:  "240x240>",
-      large:    "600x600>"
+      small:    "240x240>"
+      product:  "600x600>",
+      large:    "800x800>"
   },
 
   path:           "/spree/:class/:id/:style/:basename.:extension",
